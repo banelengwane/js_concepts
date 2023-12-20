5f7feb456c6493d40d227ed9d6f510fdfa821212
banelengwane <banelengwane@gmail.com>
2023-12-20 11:05:29 +0200

    Async / await

--- script.js
+++ script.js
@@ -297,8 +297,20 @@ const booksAfterUpdate = booksAfterDel.map((book) =>
 booksAfterUpdate;
 */
 
-fetch("https://jsonplaceholder.typicode.com/todos")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
 
+// console.log("banele");
+
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+
+  return data;
+}
+
+const todos = getTodos();
+console.log(todos);
 console.log("banele");
